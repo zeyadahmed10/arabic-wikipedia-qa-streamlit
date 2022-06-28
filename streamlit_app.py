6ce65b9858ebf8d16920c0c2a7b88cb78c383b6a
Zeyad Ahmed <zeyadahmed10101@gmail.com>
2022-06-29 01:25:28 +0200

    adding text box for context offline and check box

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import streamlit as st
 from bidi.algorithm import get_display
 
 from html_utils import ga
-from utils import annotate_answer, get_results, shorten_text
+from utils import annotate_answer, get_results, shorten_text, get_offline_results
 
 ga()
 
@@ -21,7 +21,7 @@ st.set_page_config(
 rtl = lambda w: get_display(f"{arabic_reshaper.reshape(w)}")
 
 
-_, col1, _ = st.beta_columns(3)
+_, col1, col3 ,col4= st.beta_columns([1,2,1,1])
 
 with col1:
     st.image("is2alni_logo.png", width=200)
@@ -54,16 +54,33 @@ question = st.text_input("", value="من هو جو بايدن؟")
 if "؟" not in question:
     question += "؟"
 
+st.markdown(
+        """
+    <style>
+    checkbox {
+    text-align: right;
+    }
+    </style>
+        """,
+        unsafe_allow_html=True,
+)
+offline_flag = st.checkbox(label = "ادخل قطعة بنفسك", value=False)
+if offline_flag:
+    doc = st.text_input(label = "")
 run_query = st.button("أجب")
 if run_query:
     # https://discuss.streamlit.io/t/showing-a-gif-while-st-spinner-runs/5084
     with st.spinner("... جاري البحث "):
-        results_dict = get_results(question)
+        if offline_flag:
+            results_dict = get_offline_results(question, doc)
+        else:
+            results_dict = get_results(question)
 
     if len(results_dict) > 0:
         st.write("## :الأجابات هي")
         for result in results_dict["results"][:n_answers]:
             annotate_answer(result)
-            f"[**المصدر**](<{result['link']}>)"
+            if not offline_flag:
+                f"[**المصدر**](<{result['link']}>)"
     else:
         st.write("## 😞 ليس لدي جواب")
